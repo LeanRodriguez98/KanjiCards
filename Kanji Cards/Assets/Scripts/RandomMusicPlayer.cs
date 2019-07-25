e824e95d04f49fb7a7b02ed1df13d424b1293117
Unknown <lean1998@hotmail.com>
2019-07-25 00:36:05 -0300

    AddedMusic

---
+++ Kanji Cards/Assets/Scripts/RandomMusicPlayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomMusicPlayer : MonoBehaviour {
+
+    private AudioSource audioSource;
+    public AudioClip[] audioClips;
+
+	void Start () {
+        audioSource = GetComponent<AudioSource>();
+        PlayAudio();
+
+    }
+
+    public void PlayAudio()
+    {
+        if (audioSource.isPlaying)
+        {
+            audioSource.Stop();
+        }
+
+        int index = Random.Range(0, audioClips.Length);
+        audioSource.clip = audioClips[index];
+        audioSource.Play();
+        Invoke("PlayAudio", audioClips[index].length);
+    }
+	
+
+
+}
